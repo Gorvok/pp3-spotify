46fe8237141bc467aacdb71cb2457dbe8ca9157b
Gjovani Gorvokaj <ggorvokaj@gmail.com>
2024-06-15 16:55:27 -0400

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,105 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const request = require('request');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cookieParser());
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI || 'http://localhost:3001/callback';
+
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
 app.get('/', (req, res) => {
     res.send('Backend API is running');
 });
 
+app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        res.clearCookie(stateKey);
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                const options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                request.get(options, (error, response, body) => {
+                    console.log(body);
+                });
+
+                res.redirect('/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
+        });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
